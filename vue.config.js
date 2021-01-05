d0a28e5ec944abef55da35db68627cc7e3f617f5
WieLun <wielun666@gmail.com>
2021-01-05 22:23:57 +0800

    login

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,23 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        'assets': '@/assets',
-        'common': '@/common',
-        'network': '@/network',
-        'components': '@/components',
-        'views': '@/views',
-      }
-    }
-  }
-}
+        assets: "@/assets",
+        common: "@/common",
+        network: "@/network",
+        components: "@/components",
+        views: "@/views",
+      },
+    },
+  },
+  chainWebpack(config) {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]",
+        include: ["./src/icons"],
+      });
+  },
+};
