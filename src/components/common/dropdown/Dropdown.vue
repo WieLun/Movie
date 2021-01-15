f2287c81bc391d26203bc4904602267562fe0eda
WieLun <wielun666@gmail.com>
2021-01-15 16:18:56 +0800

    modified:   src/components/common/dropdown/Dropdown.vue

--- src/components/common/dropdown/Dropdown.vue
+++ src/components/common/dropdown/Dropdown.vue
@@ -2,7 +2,8 @@
   <div class="right-menu">
     <el-dropdown>
       <span class="el-dropdown-link">
-        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+        <svg-icon icon-class="user" />
+        <span class="title">admin</span>
       </span>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>Logout</el-dropdown-item>
@@ -19,6 +20,11 @@ export default {
 
 <style scoped>
 .right-menu {
-  padding-right: 20px;
+  padding-right: 30px;
+}
+
+.title {
+  color: #409EFF;
+  padding-left: 3px;
 }
 </style>
